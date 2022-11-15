200f8524d09c0d7ec5773c2f4f51e37c2d36e20c
aalegonzalez <ale28_rc@hotmail.com>
2022-11-14 23:30:22 -0300

    Agrege cambios en la BD

--- appcoder1/admin.py
+++ appcoder1/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from appcoder1.models import *
+
 # Register your models here.
+
+admin.site.register(Curso)
+admin.site.register(Profesor)
+admin.site.register(Estudiante)
+admin.site.register(Entregable)
+
--- proyectocoder1/urls.py
+++ proyectocoder1/urls.py
@@ -20,7 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
- 
-
     path("coder/", include("appcoder1.urls"))
 ]
